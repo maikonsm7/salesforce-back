4297e13d4c5ff951f9978e53d2979dfda92ecc9e
maikonsm7 <maikonsm7@gmail.com>
2025-12-07 09:59:51 -0400

    update packages

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,10 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import "dotenv/config"
+import {PrismaPg} from '@prisma/adapter-pg'
 import { PrismaClient } from '../../generated/prisma/client.js'
 const connectionString = `${process.env.DATABASE_URL}`
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: connectionString,
-        },
-    },
-})
+const adapter = new PrismaPg({ connectionString })
+
+const prisma = new PrismaClient({ adapter })
 
 export { prisma }
